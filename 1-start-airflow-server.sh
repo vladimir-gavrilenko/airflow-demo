01ee9e7294b0d61433b60a8e8fcb6dbd0a696938
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2018-10-10 19:29:03 +0300

    modify starter scripts to start scheduler also

--- 1-start-airflow-server.sh
+++ 1-start-airflow-server.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 export AIRFLOW_HOME=airflow-home
-export AIRFLOW_GPL_UNIDECODE=yes
-
-pip install apache-airflow
-
 airflow initdb
 airflow webserver -p 8080
-airflow scheduler
---
+++ 2-start-airflow-scheduler.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export AIRFLOW_HOME=airflow-home
+airflow scheduler
